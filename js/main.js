d8464d1f12d3599482412e22dd8f1399d13ac99f
Awad2002 <awadamr33@gmail.com>
2021-08-22 11:16:35 +0300

    menu

--- js/main.js
+++ js/main.js
@@ -4,3 +4,26 @@ $(document).ready(function(){
       $(this).next('.areas').toggle('show');
     });
   });
+
+  $(document).ready(function() {
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 32) { 
+          $('.my-nav').addClass('solid');
+      } else {
+          $('.my-nav').removeClass('solid');
+      }
+    });
+});
+
+
+$(document).ready(function(){
+  $('.side-barrr').hide();
+  $('#menu-btn').on('click', function() {        
+    $('.side-barrr').toggle('show');
+  });
+  $('.side-opacity').on('click', function() {        
+    $('.side-barrr').toggle('show');
+  });
+});
